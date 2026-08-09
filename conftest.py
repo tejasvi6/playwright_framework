c04d9872f1d2e4beaea1565caa23e1f215c46f15
Tejasvi Bhimrathi <tejasvibhimrathi@gmail.com>
2026-08-09 15:35:07 +0530

    Fix Playwright for GitHub Actions

--- conftest.py
+++ conftest.py
@@ -1,12 +1,18 @@
 import pytest
+import os
 from playwright.sync_api import sync_playwright
+
 
 @pytest.fixture
 def page():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False,slow_mo=800)
+        headless = os.getenv("CI") == "true"
+
+        browser = p.chromium.launch(
+            headless=headless)
+
         page = browser.new_page()
+
         yield page
+
         browser.close()
-
-
